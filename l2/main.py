bcbd1c328ea961423f4a965b33828987683f39ed
Dimama <citrus_m@mail.ru>
2019-04-10 13:35:57 +0300

    Update main to use like command-line tool

--- l2/main.py
+++ l2/main.py
@@ -1,23 +1,33 @@
+import argparse
+
+
 from grammar_converter import GrammarConverter
 from grammar_file import GrammarFile
 
 
-if __name__ == "__main__":
-
-    # filename = input("Input filename with grammar: ")
-    filename = "grammar.json"
-
+def process_grammar(filename, action):
     try:
         g = GrammarFile.create_grammar_from_file(filename)
     except Exception as e:
         print(e)
     else:
-        g_without_recursion = GrammarConverter.delete_left_recursion(g)
-        g_without_eps_productions = GrammarConverter.delete_eps_productions(g)
-
-        GrammarFile.save_grammar_to_file(g_without_recursion, "grammar_without_recursion.json")
-        GrammarFile.save_grammar_to_file(g_without_eps_productions, "grammar_without_eps_productions.json")
-
         print("Loaded grammar:\n{}".format(g))
-        print("\nWithout left recursion:\n{}".format(g_without_recursion))
-        print("\nWithout eps productions:\n{}".format(g_without_eps_productions))
+        if action == "eps":
+            grammar = GrammarConverter.delete_eps_productions(g)
+            GrammarFile.save_grammar_to_file(grammar, "grammar_without_eps_productions.json")
+            print("\nWithout eps productions:\n{}".format(grammar))
+        elif action == "left-rec":
+            grammar = GrammarConverter.delete_left_recursion(g)
+            GrammarFile.save_grammar_to_file(grammar, "grammar_without_recursion.json")
+            print("\nWithout left recursion:\n{}".format(grammar))
+        else:
+            raise Exception("Unknown type of action")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Remove eps-productions and left recursion")
+    parser.add_argument("action", type=str, choices=["eps", "left-rec"], help="eps or left-rec")
+    parser.add_argument("filename", type=str, help="File with grammar")
+    args = parser.parse_args()
+    process_grammar(args.filename, args.action)
+
